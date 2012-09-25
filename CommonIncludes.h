3c9fb4858d6f35b41bfa52a934be69a286ca133f
Federico Gandellini <fgandellini@gmail.com>
2012-09-26 00:52:50 +0200

    inclusi files di progetto eclipse

--- CommonIncludes.h
+++ CommonIncludes.h
@@ -3,5 +3,5 @@
 using namespace std;
 
 #define SAFE_DELETE(x) if (x != NULL) { delete x; }
-#define SAFE_DELETE_VECTOR(v) if (v != NULL) { for (int i=0; i<v->size(); ++i) { SAFE_DELETE(v->at(i)); } SAFE_DELETE(v); }
+#define SAFE_DELETE_VECTOR(v) if (v != NULL) { for (unsigned int i=0; i<v->size(); ++i) { SAFE_DELETE(v->at(i)); } SAFE_DELETE(v); }
 
